e1dd313c5a7dd09c371df1e3d3ba3213d44ae26c
KAldhalimi <karrarmajed@gmail.com>
2019-02-21 23:54:23 -0500

    initial commit

---
+++ APILab/Models/Links.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APILab.Models
+{
+    public class Links
+    {
+        public string Title { get; set; }
+        public string ImageURL { get; set; }
+        public string LinkURL { get; set; }
+        public string data { get; set; }
+    }
+}
